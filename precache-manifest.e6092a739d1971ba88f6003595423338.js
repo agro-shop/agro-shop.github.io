f6e339536618e4d2cfa1a0ae007b160e24b87157
Akshay Patil <akshay.patil9111999@gmail.com>
2020-12-11 19:34:17 +0530

    commit1

---
+++ precache-manifest.e6092a739d1971ba88f6003595423338.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19cc60ca2539144eb0ef11338ed1463",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dab61db80c6e2de554a",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "c8bd511256c7a36f39e1",
+    "url": "/static/css/main.776ae5d0.chunk.css"
+  },
+  {
+    "revision": "9dab61db80c6e2de554a",
+    "url": "/static/js/2.6da9411b.chunk.js"
+  },
+  {
+    "revision": "b84c0c8ded84d686f9a9a77bebeb025c",
+    "url": "/static/js/2.6da9411b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8bd511256c7a36f39e1",
+    "url": "/static/js/main.4bd8e4e1.chunk.js"
+  },
+  {
+    "revision": "f7716720fa9334923298",
+    "url": "/static/js/runtime-main.22f83426.js"
+  },
+  {
+    "revision": "5545ace553c866178260c11be7daf253",
+    "url": "/static/media/icon.5545ace5.png"
+  },
+  {
+    "revision": "3a4f6cd4e9d5d4302fb98e536df36af6",
+    "url": "/static/media/news.3a4f6cd4.png"
+  }
+]);
